c0483a52958032e1d3cdcc60f01d91a509f86a37
oj <ojcc1992@gmail.com>
2022-10-04 12:14:01 +0100

    removestufff

--- azscriptgetRGinfo.ps1
+++ azscriptgetRGinfo.ps1
@@ -6,7 +6,6 @@ $RGNAME = "`"$($RGReal.Name)`""
 $RGID = "`"$($RGReal.ID)`""
 $RGLOC = "`"$($RGReal.Location)`""
 
-$NameString
 Set-Content -Path containeringtf\modules\Core\terraform.tfvars -Value "rg_name = $RGNAME", "rg_id = $RGID", "rg_location = $RGLOC" -PassThru
 
 Set-Content -Path remote-state\terraform.tfvars -Value "rg_name = $RGNAME", "rg_id = $RGID", "rg_location = $RGLOC" -PassThru
